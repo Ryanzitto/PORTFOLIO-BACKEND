2b41d800a3523d953e604f166cd8e72d7c623052
Ryan Henrique <ryanhardflip@gmail.com>
2023-10-02 20:54:45 -0300

    bug no DB

--- src/db/conn.js
+++ src/db/conn.js
@@ -4,17 +4,14 @@ const dbUser = process.env.DB_USER
 const dbPass = process.env.DB_PASS
 
 async function main(){
-
     try {
         mongoose.set("strictQuery", true)
-        await mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.9tccmlj.mongodb.net/?retryWrites=true&w=majority`)
+        await mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster99.vlfbz5i.mongodb.net/?retryWrites=true&w=majority`)
         
         console.log("conectado ao banco")
     } catch (error) {
         console.log(error)
     }
 }
-
-
 
 module.exports = main
